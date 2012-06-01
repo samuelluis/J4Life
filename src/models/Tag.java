0c459b5ed04df2b22b284b1ac2638713c6a18ab8
Samuel Luis De La Cruz <sammultiservices@gmail.com>
2012-06-01 15:45:07 -0400

    Model Tag Inproved

--- src/models/Tag.java
+++ src/models/Tag.java
@@ -1,28 +1,29 @@
 package models;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import utils.DateHelper;
 import utils.Logger;
 
 public class Tag implements IModel {
 
 	private int id;
 	private String name;
+	private Date createdAt;
+	private Date updatedAt;
 	
-	public Tag() {
-		this(null);
+	private Tag() {
+		this("");
 	}
 
 	public Tag(String name) {
 		super();
 		this.id = 0;
 		this.name = name;
-	}
-	
-	public static Tag create() {
-		return create(null);
 	}
 
 	public static Tag create(String name) {
@@ -33,14 +34,15 @@ public class Tag implements IModel {
 	public int getId() {
 		return id;
 	}
-
-	public void setId() {
-		try { 
+	
+	private void afterSave() {
+		try {
 			ResultSet result = Connection.getConnection().excecuteQuery("select max(id) from tags");
-			result.next();
-			this.id = result.getInt(1);
-		}
-		catch (Exception e) {}
+			id = result.getInt(1);
+			Tag tag = Tag.find(id);
+			this.createdAt = tag.createdAt;
+			this.updatedAt = tag.updatedAt;
+		} catch (SQLException e) {}
 	}
 
 	@Override
@@ -52,6 +54,40 @@ public class Tag implements IModel {
 		this.name = name;
 	}
 	
+	public boolean addProduct(Product product){
+		return addProduct(product.getId());
+	}
+
+	public boolean addProduct(int productId){
+		try { Connection.getConnection().excecuteUpdate("insert into products_tags(`tag_id`,`product_id`) values("+id+","+productId+")"); return true; }
+		catch (Exception e) { Logger.severe("Couldn't Add Product: ".concat(productId+"").concat("\nReason: ".concat(e.toString()))); return false;}
+	}
+	
+	public boolean removeProduct(Product product){
+		return addProduct(product.getId());
+	}
+
+	public boolean removeProduct(int productId){
+		try { Connection.getConnection().excecuteUpdate("delete from products_tags where tag_id="+id+" and product_id="+productId); return true; }
+		catch (Exception e) { Logger.severe("Couldn't Remove Product: ".concat(productId+"").concat("\nReason: ".concat(e.toString()))); return false;}
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
 	public static Tag find(int id){
 		Tag tag = new Tag();
 		try { 
@@ -59,11 +95,13 @@ public class Tag implements IModel {
 			if(result.next()){
 				tag.id = id;
 				tag.name = result.getString("name");
+				tag.createdAt = result.getDate("created_at");
+				tag.updatedAt = result.getDate("updated_at");
 			}
 			else
-				Logger.warning("Couldn't find tag with id: "+id);
+				Logger.warning("Couldn't find Tag with id: "+id);
 		}
-		catch (Exception e) { Logger.severe("Couldn't complete the search: "+e.toString()); }
+		catch (Exception e) { Logger.severe("Couldn't complete the search of Tag: "+e.toString()); }
 		
 		return tag;
 	}
@@ -73,16 +111,25 @@ public class Tag implements IModel {
 		try {
 			ResultSet result = Connection.getConnection().excecuteQuery("select id from tags");
 			while(result.next()) tags.add(find(result.getInt("id")));
-		} catch (Exception e) { Logger.severe("Couldn't complete the search: "+e.toString()); }
+		} catch (Exception e) { Logger.severe("Couldn't complete the search of Tags: "+e.toString()); }
 		return tags;
+	}
+	
+	public List<Product> getProducts(){
+		List<Product> products = new ArrayList<Product>();
+		try {
+			ResultSet result = Connection.getConnection().excecuteQuery("select product_id from products_tags where tag_id="+id);
+			while(result.next()) products.add(Product.find(result.getInt("product_id")));
+		} catch (Exception e) { Logger.severe("Couldn't complete the search of Products: "+e.toString()); }
+		return products;
 	}
 
 	@Override
 	public boolean save() {
 		String sql = "";
-		if(id==0) sql = "insert into tags(`name`) values('"+name+"')";
-		else sql = "update tags set name='"+name+"' where id="+id;
-		try { Connection.getConnection().excecuteUpdate(sql); if(id==0) setId(); return true; }
+		if(id==0) sql = "insert into tags(`name`,`created_at`,`updated_at`) values('"+name+"','"+DateHelper.getDateTimeString(new Date())+"','"+DateHelper.getDateTimeString(new Date())+"')";
+		else sql = "update tags set name='"+name+"', updated_at='"+DateHelper.getDateTimeString(new Date())+"' where id="+id;
+		try { Connection.getConnection().excecuteUpdate(sql); if(id==0) afterSave(); return true; }
 		catch (Exception e) { Logger.severe("Couldn't Save: ".concat(attributes()).concat("\nReason: ".concat(e.toString()))); return false;}
 	}
 
@@ -93,13 +140,13 @@ public class Tag implements IModel {
 	}
 	
 	public static void destroyAll(){
-		for(Tag tag : Tag.all())
-			tag.destroy();
+		for(Category category : Category.all())
+			category.destroy();
 	}
 
 	@Override
 	public String attributes() {
-		return "tags [ id="+id+", name="+name+" ]";
+		return "Tag [ id="+id+", name='"+name+"', created_at='"+DateHelper.getDateTimeString(createdAt)+"', updated_at='"+DateHelper.getDateTimeString(updatedAt)+"' ]";
 	}
 	
 	@Override
